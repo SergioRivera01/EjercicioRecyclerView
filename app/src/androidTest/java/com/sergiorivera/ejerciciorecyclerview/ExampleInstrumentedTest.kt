c22315caa83c0d11f81766a15f691cedfcecdd1d
SergioRivera01 <sergiorivera01@gmail.com>
2021-11-08 11:55:39 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/sergiorivera/ejerciciorecyclerview/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.sergiorivera.ejerciciorecyclerview
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.sergiorivera.ejerciciorecyclerview", appContext.packageName)
+    }
+}
